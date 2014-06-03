ef4b1ed62880320b2a6fb15623a50d4d6b47c608
berpj <berpj@users.noreply.github.com>
2014-06-03 23:01:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Pierre-Jean Bergeron</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pierre-Jean Bergeron">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" rel="stylesheet">
+</head>
+<body>
+    <div id="wrap">
+        <div class="container">
+            <div class="page-header">
+                <h1>Pierre-Jean Bergeron</h1>
+            </div>
+            <p class="lead">21 ans, étudiant en informatique à Epitech Lyon, et passionné par Internet.</p>
+            <p class="lead">Je suis le fondateur de <a href="https://votrevpn.com">VotreVPN</a>, fournisseur d'accès VPN français.</p>
+            <div id="links">
+                <a target="_BLANK" href="https://twitter.com/berpj"><i class="fa fa-twitter fa-3x"></i></a>
+                <a target="_BLANK" href="https://github.com/berpj"><i class="fa fa-github-alt fa-3x"></i></a>
+                <a target="_BLANK" href="http://fr.linkedin.com/in/pierrejeanbergeron/"><i class="fa fa-linkedin fa-3x"></i></a>
+                <a target="_BLANK" href="http://www.google.com/recaptcha/mailhide/d?k=01cMPwhOhIdVTQ_KygiLMvTw==&c=iS-bsLu649AoM-Bc90LsM342GIj7VtelMrDfUA51Y7g="><i class="fa fa-envelope fa-3x"></i></a>
+            </div>
+        </div>
+    </div>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-28397864-2', 'pierrejeanbergeron.fr');
+    ga('send', 'pageview');
+    </script>
+</body>
+</html>
